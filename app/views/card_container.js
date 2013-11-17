1842ec09c7046f134c9c3e164e07b98a972c5dd7
Wutian <naituw@gmail.com>
2013-11-17 23:04:19 +0800

    Add CardContainerView's page show style detection

--- app/views/card_container.js
+++ app/views/card_container.js
@@ -1,6 +1,9 @@
 var CardContainerView = Ember.View.extend({
-	classNames: ['card-container'],
+	classNameBindings: [':card-container','isBubble:bubble-style:default-style'],
 	templateName: 'card_container',
+	isBubble: function(){
+		return this.get('page.show_style') === 1;
+	}.property('page.show_style'),
 });
 
 export default CardContainerView;
