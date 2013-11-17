af85c5ccada89f527ae8e531593b8bf72085a7ce
Wutian <naituw@gmail.com>
2013-11-17 23:04:39 +0800

    Add ConfigSelect's targetModel binding

--- app/views/config_select.js
+++ app/views/config_select.js
@@ -3,6 +3,24 @@ var ConfigSelectView = Ember.Select.extend({
 	optionLabelPath: 'content.title',
 	optionValuePath: 'content.value',
 	contentBinding: 'config.options',
+	bindingPath: function(){
+		var path = this.get('config.path');
+		return 'targetModel.' + path;
+	}.property('config.path'),
+	valueChanged: function(){
+		var path = this.get('config.path');
+		var target = this.get('targetModel');
+		target.set(path, this.get('value'));
+	}.observes('value'),
+	didInsertElement: function(){
+		var that = this;
+		var updateCurrentValue = function(){
+			var current = that.get(that.get('bindingPath'));
+			that.set('value', current);
+		};
+		updateCurrentValue();
+		that.addObserver(that.get('bindingPath'), updateCurrentValue);
+	},
 });
 
 export default ConfigSelectView;
