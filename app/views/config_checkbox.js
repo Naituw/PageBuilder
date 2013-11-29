3aac7bb0ca6b04676406c6556fd55f5724dc9375
Wutian <naituw@gmail.com>
2013-11-29 09:53:39 +0800

    ConfigCheckbox now supports automatic disable

--- app/views/config_checkbox.js
+++ app/views/config_checkbox.js
@@ -1,10 +1,15 @@
 var ConfigCheckbox = Ember.Checkbox.extend({
 	classNames: ['config-checkbox'],
 	elementInserted: false,
+	attributeBindings: ['disabled'],
 	bindingPath: function(){
 		var path = this.get('config.path');
 		return 'targetModel.' + path;
 	}.property('config.path'),
+	disablePath: function(){
+		var path = this.get('config.disable.path');
+		return 'targetModel.' + path;
+	}.property('config.disable.path'),
 	valueChanged: function(){
 		if (!this.get('elementInserted')) return;
 		var path = this.get('config.path');
@@ -16,10 +21,20 @@ var ConfigCheckbox = Ember.Checkbox.extend({
 		var current = that.get(that.get('bindingPath'));
 		that.set('checked', !!current);
 	},
+	updateDisableValue: function(){
+		var current = this.get(this.get('disablePath'));
+		var value = this.get('config.disable.value');
+		this.set('disabled', current === value);
+	},
 	didInsertElement: function(){
 		var that = this;
 		this.updateCurrentValue();
 		that.addObserver(that.get('bindingPath'), this.updateCurrentValue);
+
+		if (this.get('config.disable')) {
+			this.updateDisableValue();
+			this.addObserver(this.get('disablePath'), this.updateDisableValue);
+		}
 
 		Em.run.next(this, function(){
 			this.set('elementInserted', true);
@@ -27,6 +42,7 @@ var ConfigCheckbox = Ember.Checkbox.extend({
 	},
 	willDestroyElement: function(){
 		this.removeObserver(this.get('bindingPath'), this.updateCurrentValue);
+		this.removeObserver(this.get('disablePath'), this.updateDisableValue);
 	},
 });
 
