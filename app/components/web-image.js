57efd44922ad872a013b032bbe7eda43094b1cd8
Wutian <naituw@gmail.com>
2013-12-05 13:53:20 +0800

    Web Image should transparent its background if there is a url

--- app/components/web-image.js
+++ app/components/web-image.js
@@ -3,7 +3,9 @@ var WebImage = Ember.Component.extend({
     classNames: ['card-web-image'],
     attributeBindings: ['style'],
     style: function(){
-        return 'width:' + this.get('width') + 'px; height:' + this.get('height') + 'px;' + 'background-image:url(' + this.get('url') + ')';
+        var s = 'width:' + this.get('width') + 'px; height:' + this.get('height') + 'px;' + 'background-image:url(' + this.get('url') + ');';
+        if (this.get('url.length')) s += 'background-color:transparent;';
+        return s;
     }.property('width', 'height', 'url'),
     width: 50,
     height: 50,
